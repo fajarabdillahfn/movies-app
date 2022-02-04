5206b12e58595062c50cb6dd00f04047a987e20d
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-02-04 13:48:11 +0700

    add categories page and finalize front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,104 @@
+import React, { Component, Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom';
+import Movies from './components/Movies';
+import Home from './components/Home';
+import Admin from './components/Admin';
+import Categories from './components/Categories';
+import { useRouteMatch } from 'react-router';
 
-function App() {
+export default function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <h1 className="mt-3">
-          Go Watch a Movie!
-        </h1>
-        <hr className="mb-3" />
-      </div>
-      <div className="row">
-        <div className="col-md-2">
-          <nav>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <a href="/">Home</a>
-              </li>
-              <li className="list-group-item">
-                <a href="/movies">Movies</a>
-              </li>
-              <li className="list-group-item">
-                <a href="/admin">Manage Catalogue</a>
-              </li>
-            </ul>
-          </nav>
+    <Router>
+      <div className='container'>
+        <div className='row'>
+          <h1 className='mt-3'>Go Watch a Movie!</h1>
+          <hr className='mb-3'></hr>
         </div>
-        <div className="col-md-10"></div>
+
+        <div className='row'>
+          <div className='col-md-2'>
+            <nav>
+              <ul className='list-group'>
+                <li className='list-group-item'>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to='/movies'>Movies</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to='/by-category'>Categories</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to='/admin'>Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className='col-md-10'>
+            <Switch>
+              <Route path='/movies/:id'>
+                <Movie />
+              </Route>
+              <Route path='/movies'>
+                <Movies />
+              </Route>
+              <Route exact path='/by-category'>
+                <CategoryPage />
+              </Route>
+              <Route
+                exact
+                path='/by-category/drama'
+                render={(props) => (
+                  <Categories {...props} title={`Drama`} />
+                )}></Route>
+                <Route
+                exact
+                path='/by-category/comedy'
+                render={(props) => (
+                  <Categories {...props} title={`Comedy`} />
+                )}></Route>
+              <Route path='/admin'>
+                <Admin />
+              </Route>
+              <Route path='/'>
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
-export default App;
+function Movie() {
+  let { id } = useParams();
+
+  return <h2>Movie id: {id}</h2>;
+}
+
+function CategoryPage() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Categories</h2>
+
+      <ul>
+        <li>
+          <Link to={`${path}/comedy`}>Comedy</Link>
+        </li>
+        <li>
+          <Link to={`${url}/drama`}>Drama</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
