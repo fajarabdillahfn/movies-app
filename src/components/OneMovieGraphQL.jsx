705e173d7eb7a77710d1410034b8434792b71dc1
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-02-11 15:29:36 +0700

    show poster

--- src/components/OneMovieGraphQL.jsx
+++ src/components/OneMovieGraphQL.jsx
@@ -19,6 +19,7 @@ export default class OneMovieGraphQL extends Component {
             release_date
             rating
             mpaa_rating
+            poster
         }
     }
     `;
@@ -61,6 +62,15 @@ export default class OneMovieGraphQL extends Component {
           <h2>
             Movie: {movie.title} ({movie.year})
           </h2>
+
+          {movie.poster !== '' && (
+            <div>
+              <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster}`}
+                alt='poster'
+              />
+            </div>
+          )}
 
           <div className='float-start'>
             <small>Rating: {movie.mpaa_rating}</small>
